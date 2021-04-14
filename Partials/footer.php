89bcbfd464aae038ae4f29356a49a2b38cc4d0e1
Leonora Mavriqi <lm43877@ubt-uni.net>
2021-04-14 13:48:07 +0200

    .

--- Partials/footer.php
+++ Partials/footer.php
@@ -1,5 +1,5 @@
 <footer>
-        2020
+        2021
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous"></script>
